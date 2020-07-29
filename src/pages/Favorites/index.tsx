70a5e198f13ede31e39e6ffe60419f899eafce9d
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-07-29 20:15:41 -0300

    remove Favorite buttom from Favorites component

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import CardItem from "../../components/CardItem";
-import Favorite from "../../components/UI/Favorite";
 
 const Favorites = () => {
   const [favored, setFavored] = useState(
@@ -18,17 +17,7 @@ const Favorites = () => {
       }}
     >
       {favored.map((pokemon: string) => (
-        <div
-          key={pokemon}
-          style={{
-            display: "flex",
-            alignItems: "center",
-            flexDirection: "column",
-          }}
-        >
           <CardItem name={pokemon} />
-          <Favorite pokemonName={pokemon} />
-        </div>
       ))}
     </div>
   );
