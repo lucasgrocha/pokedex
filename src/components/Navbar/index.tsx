cb71998f5fa8abc8689dc962f67527ada3f5a419
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-07-29 19:06:54 -0300

    add Favorites page to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,23 @@
 import React from "react";
-import { Nav, Logo, Title, StyledLink } from "./styles";
+import { Nav, Logo, Title, StyledLink, TabLink, Tabs } from "./styles";
 import pokeball from "../../assets/pokeball.svg";
 
 const navbar = () => {
   return (
     <Nav>
-      <StyledLink to="/">
-        <Logo src={pokeball} alt="Home" title="Home" />
-        <Title>Pokedex</Title>
-      </StyledLink>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <StyledLink to="/">
+          <Logo src={pokeball} alt="Home" title="Home" />
+          <Title>Home</Title>
+        </StyledLink>
+        <Title>&nbsp;|&nbsp;</Title>
+        <StyledLink to="/favorites">
+          <Title>Favorites</Title>
+        </StyledLink>
+      </div>
+      <Tabs>
+        <TabLink to="/favorites">Favorites</TabLink>
+      </Tabs>
     </Nav>
   );
 };
--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -3,13 +3,14 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const Nav = styled.nav`
-  padding: 10px 0;
+  padding-top: 10px;
   display: flex;
   background-color: #ff7c88;
 
   @media screen and (min-width: 565px) {
     justify-content: center;
     align-items: center;
+    flex-direction: column;
   }
 
   @media screen and (max-width: 565px) {
@@ -19,11 +20,12 @@ export const Nav = styled.nav`
 
 export const Logo = styled.img`
   width: 40px;
+  margin-bottom: 10px;
+  margin-right: 10px;
 `;
 
 export const Title = styled.h5`
   display: none;
-  margin-left: 10px;
 
   @media screen and (max-width: 565px) {
     display: initial;
@@ -35,5 +37,31 @@ export const StyledLink = styled(Link)`
   &:hover {
     color: black;
     text-decoration: none;
+  }
+`;
+
+export const TabLink = styled(Link)`
+  color: black;
+  background-color: #ffe01f;
+  padding: 5px;
+  border-radius: 5px;
+  height: 25px;
+  line-height: 14.5px;
+  &:hover {
+    color: black;
+    text-decoration: none;
+  }
+`;
+
+export const Tabs = styled.div`
+  background-color: #ac1515;
+  width: 100%;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  @media screen and (max-width: 565px) {
+    display: none;
   }
 `;
